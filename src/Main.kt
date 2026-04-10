c7977780985d5fd25b5c7ef65fb792d90a3c97c2
Dan <vfvf5389127@gmail.com>
2026-04-10 15:20:18 +0300

    Create sealed model ModuleResult

--- src/Main.kt
+++ src/Main.kt
@@ -2,6 +2,18 @@ import modules.EnergyGenerator
 import modules.ResearchLab
 import resources.OutpostResource
 import resources.ResourceManager
+import modules.ModuleResult
+
+fun handleModuleResult(result: ModuleResult) {
+    when (result) {
+        is ModuleResult.Success -> println("УСПЕХ: ${result.message}")
+        is ModuleResult.ResourceProduced -> println("Произведено: ${result.resourceName} + ${result.amount}")
+        is ModuleResult.NotEnoughResources -> println(
+            "Недостаточно ресурса ${result.resourceName}. " + "Нужно: ${result.required}, есть: ${result.available}"
+        )
+        is ModuleResult.Error -> println("ОШИБКА: ${result.reason}")
+    }
+}
 
 fun main() {
     val manager = ResourceManager()
@@ -14,12 +26,14 @@ fun main() {
 //
 //    val bonus = minerals.copy(amount = minerals.amount + 50)
 //    println("Копия минералов с бонусом: $bonus")
-    manager.add(OutpostResource(1, "Minerals", 120))
-    manager.add(OutpostResource(2, "Gas", 40))
+    manager.add(OutpostResource(1, "Minerals", 300))
+    manager.add(OutpostResource(2, "Gas", 100))
     val generator = EnergyGenerator()
     val lab = ResearchLab()
-    generator.performAction(manager)
-    lab.performAction(manager)
+    val generatorResult = generator.performAction(manager)
+    val labResult = lab.performAction(manager)
+    handleModuleResult(generatorResult)
+    handleModuleResult(labResult)
     println()
     manager.printAll()
 }
--- src/modules/EnergyGenerator.kt
+++ src/modules/EnergyGenerator.kt
@@ -1,16 +1,20 @@
 package modules
 
-import resources.OutpostResource
 import resources.ResourceManager
+import modules.OutpostModule
+import resources.OutpostResource
 
 class EnergyGenerator : OutpostModule("Генератор энергии") {
-    override fun performAction(manager: ResourceManager) {
+    override fun performAction(manager: ResourceManager) : ModuleResult {
         println("генератор работает... производит 20 энергии")
         val energy = manager.get("Energy")
-        if (energy != null) {
-            energy.amount += 20
-        } else {
-            manager.add(OutpostResource(99, "Energy", 20))
+
+            return if (energy != null) {
+                energy.amount += 20
+                ModuleResult.ResourceProduced("Enery", 20)
+            } else {
+                manager.add(OutpostResource(99, "Energy", 20))
+                ModuleResult.Success("Энергия создана впервые")
+            }
         }
     }
-}
